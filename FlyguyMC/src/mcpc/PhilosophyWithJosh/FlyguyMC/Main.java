63b5abd6d28f53230f15f174344662bf4621f452
PhilosophyWithJosh <joshmendiola@icloud.com>
2022-04-29 18:36:27 -0500

    added a command

--- FlyguyMC/src/mcpc/PhilosophyWithJosh/FlyguyMC/Main.java
+++ FlyguyMC/src/mcpc/PhilosophyWithJosh/FlyguyMC/Main.java
@@ -2,11 +2,13 @@ package mcpc.PhilosophyWithJosh.FlyguyMC;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+import mcpc.PhilosophyWithJosh.FlyguyMC.commands.TntCommand;
+
 public class Main extends JavaPlugin
 {
 	@Override
 	public void onEnable()
 	{
-		
+		new TntCommand(this);
 	}
 }
--- FlyguyMC/src/mcpc/PhilosophyWithJosh/FlyguyMC/commands/TntCommand.java
+++ FlyguyMC/src/mcpc/PhilosophyWithJosh/FlyguyMC/commands/TntCommand.java
@@ -1,8 +1,13 @@
 package mcpc.PhilosophyWithJosh.FlyguyMC.commands;
 
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Player;
 
 import mcpc.PhilosophyWithJosh.FlyguyMC.Main;
 
@@ -16,8 +21,40 @@ public class TntCommand implements CommandExecutor
 		plugin.getCommand("flyguy").setExecutor(this);
 	}
 	
-	public boolean onCommand(CommandSender sender, Command beblockhunter, String label, String[] args)
+	public boolean onCommand(CommandSender sender, Command flyguy, String label, String[] args)
 	{
-		return true;
+		if(sender instanceof Player)
+		{
+			Player p = (Player)sender;	
+			
+			Location playerLoc = p.getLocation().clone();
+			Location tntLoc = p.getLocation().clone().subtract(0,1,0);
+			Location redLoc = p.getLocation().clone().subtract(0,2,0);
+			Location obsidianOne = playerLoc.subtract(1,0,0);
+			Location obsidianTwo = playerLoc.subtract(0,0,1);
+			Location obsidianThree = playerLoc.add(1,0,0);
+			Location obsidianFour = playerLoc.add(0,0,1);
+			
+			Block blockOne = obsidianOne.getBlock();
+			blockOne.setType(Material.OBSIDIAN);
+			Block blockTwo = obsidianTwo.getBlock();
+			blockTwo.setType(Material.OBSIDIAN);
+			Block blockThree = obsidianThree.getBlock();
+			blockThree.setType(Material.OBSIDIAN);
+			Block blockFour = obsidianFour.getBlock();
+			blockFour.setType(Material.OBSIDIAN);
+			
+			Block waterBlock = playerLoc.getBlock();
+			waterBlock.setType(Material.WATER);
+			Block tntBlock = tntLoc.getBlock();
+			tntBlock.setType(Material.TNT);
+			Block redBlock = redLoc.getBlock();
+			redBlock.setType(Material.REDSTONE_BLOCK);
+			
+			p.sendMessage("Have fun");
+			return true;
+			
+		}
+		return false;
 	}
 }
